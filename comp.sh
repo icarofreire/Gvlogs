ec375b637816558db13758933cc78dc0ccf1dd85
icarofreire <icaro.ns@hotmail.com>
2016-07-19 13:28:47 -0300

    renomeação

--- comp.sh
+++ comp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 clear
-g++ -std=c++11 main.cpp -o helloworld `pkg-config gtkmm-3.0 --cflags --libs`
+g++ -std=c++11 main.cpp -o Gvlogs `pkg-config gtkmm-3.0 --cflags --libs`
--- main.cpp
+++ main.cpp
@@ -16,16 +16,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#include "helloworld.h"
-#include "helloworld.cpp"
+#include "gvlogs.h"
+#include "gvlogs.cpp"
 #include <gtkmm/application.h>
 
 int main (int argc, char *argv[])
 {
   Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-  HelloWorld helloworld;
+  Gvlogs gvlogs;
 
   //Shows the window and returns when it is closed.
-  return app->run(helloworld);
+  return app->run(gvlogs);
 }
