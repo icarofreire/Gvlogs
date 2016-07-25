ec32416458bfc1ac438abd5e2e08ccab17ec166e
icarofreire <icaro.ns@hotmail.com>
2016-07-25 15:06:08 -0300

    resolvido bug remover aba e add uma nova aba.

--- gvlogs.cpp
+++ gvlogs.cpp
@@ -21,47 +21,43 @@
 #include <vector>
 using namespace std;
 
-Gvlogs::Gvlogs()
-: m_button("Hello World"),   // creates a new button with label "Hello World".
-  m_VBox(Gtk::ORIENTATION_VERTICAL),
-  m_Button_Quit("Sair")
+Gvlogs::Gvlogs():
+	m_button("Hello World"),   // creates a new button with label "Hello World".
+	m_VBox(Gtk::ORIENTATION_VERTICAL),
+	m_Button_Quit("Sair")
 {
-  // Sets the border width of the window.
-  set_border_width(10);
-  set_title("Visualizador de logs");
-  set_default_size(largura, altura);
-
-  add_arquivo.set_label("Adicionar arquivo");
-  remover_aba.set_label("Remover Aba");
-
-  add(m_VBox);
-
- //Add the Notebook, with the button underneath:
- m_Notebook.set_border_width(10);
- m_VBox.pack_start(m_Notebook);
- m_VBox.pack_start(m_ButtonBox, Gtk::PACK_SHRINK);
-
- m_ButtonBox.pack_start(add_arquivo, Gtk::PACK_SHRINK);
- m_ButtonBox.pack_start(remover_aba, Gtk::PACK_SHRINK);
- m_ButtonBox.pack_start(m_Button_Quit, Gtk::PACK_SHRINK);
-
- m_Button_Quit.signal_clicked().connect(sigc::mem_fun(*this,
-             &Gvlogs::on_button_quit) );
-
- add_arquivo.signal_clicked().connect(sigc::mem_fun(*this,
-             &Gvlogs::evento_botao_add_arquivo) );
-
- remover_aba.signal_clicked().connect(sigc::mem_fun(*this,
-             &Gvlogs::evento_botao_remover_aba) );
-
- m_Notebook.signal_switch_page().connect(sigc::mem_fun(*this,
-             &Gvlogs::evento_trocou_aba) );
-
- vector<Glib::ustring> l__;
- // add_aba(titulo_primeira_aba, &l__);
-
-
- show_all_children();
+	// Sets the border width of the window.
+	set_border_width(10);
+	set_title("Visualizador de logs");
+	set_default_size(largura, altura);
+	
+	add_arquivo.set_label("Adicionar arquivo");
+	remover_aba.set_label("Remover Aba");
+	
+	add(m_VBox);
+	
+	//Add the Notebook, with the button underneath:
+	m_Notebook.set_border_width(10);
+	m_VBox.pack_start(m_Notebook);
+	m_VBox.pack_start(m_ButtonBox, Gtk::PACK_SHRINK);
+	
+	m_ButtonBox.pack_start(add_arquivo, Gtk::PACK_SHRINK);
+	m_ButtonBox.pack_start(remover_aba, Gtk::PACK_SHRINK);
+	m_ButtonBox.pack_start(m_Button_Quit, Gtk::PACK_SHRINK);
+	
+	m_Button_Quit.signal_clicked().connect(sigc::mem_fun(*this, &Gvlogs::on_button_quit) );
+	
+	add_arquivo.signal_clicked().connect(sigc::mem_fun(*this, &Gvlogs::evento_botao_add_arquivo) );
+	
+	remover_aba.signal_clicked().connect(sigc::mem_fun(*this, &Gvlogs::evento_botao_remover_aba) );
+	
+	m_Notebook.signal_switch_page().connect(sigc::mem_fun(*this, &Gvlogs::evento_trocou_aba) );
+	
+	vector<Glib::ustring> l__;
+	// add_aba(titulo_primeira_aba, &l__);
+	
+	
+	show_all_children();
 
 }
 
@@ -76,120 +72,134 @@ void Gvlogs::evento_trocou_aba(Gtk::Widget* page, guint page_num)
 
 void Gvlogs::evento_botao_add_arquivo()
 {
-  // std::cout << "Add um novo arquivo" << std::endl;
-
-  abrir_seletor_de_arquivos();
-  if( (!caminho_completo_arquivo.empty()) && (!nome_arquivo.empty()) )
-  {
-    // std::cout << "File selected: " <<  caminho_completo_arquivo << std::endl;
-    vector<Glib::ustring> linhas = linhas_arquivo(caminho_completo_arquivo);
-
-    int tam_max = 30;
-    if( nome_arquivo.size() > tam_max ){
-      nome_arquivo = nome_arquivo.substr(0,tam_max-3)+"...";
-    }
-    add_aba(nome_arquivo, &linhas);
-  }
-
+	abrir_seletor_de_arquivos();
+	if( (!caminho_completo_arquivo.empty()) && (!nome_arquivo.empty()) )
+	{
+		vector<Glib::ustring> linhas = linhas_arquivo(caminho_completo_arquivo);
+		int tam_max = 30;
+		if( nome_arquivo.size() > tam_max ){
+			nome_arquivo = nome_arquivo.substr(0,tam_max-3)+"...";
+		}
+		add_aba(nome_arquivo, &linhas);
+	}
 }
 
 void Gvlogs::evento_botao_remover_aba()
 {
-  int aba = m_Notebook.get_current_page();
-  std::cout << "remover aba: " << aba << std::endl;
-  if( m_Notebook.get_n_pages() > 1 ){
-    // vector_delete(aba);
-    m_Notebook.remove_page(aba);
-  }
+	int aba = m_Notebook.get_current_page();
+	if( m_Notebook.get_n_pages() > 1 ){
+		m_Notebook.remove_page(aba);
+	}
 }
 
 void Gvlogs::on_button_quit()
 {
-  hide();
+	hide();
 }
 
 void Gvlogs::add_aba(Glib::ustring titulo_da_aba, vector<Glib::ustring>* linhas)
 {
-  int indice_nova_aba = m_Notebook.get_n_pages();
-  add_lista_em_aba(indice_nova_aba, *linhas);
-
-  // if( (m_Notebook.get_tab_label_text(scroll[0]) == titulo_primeira_aba) && (m_Notebook.get_n_pages() == 1) ){
-      // m_Notebook.remove_page(0);
-  // }
-
-  m_Notebook.append_page(scroll[indice_nova_aba], titulo_da_aba);
-  lista.show_all();
-  show_all_children();
-  m_Notebook.set_current_page(indice_nova_aba);
+	int indice_nova_aba = m_Notebook.get_n_pages();
+	int scroll_aba = add_lista_em_aba(*linhas);
+	
+	m_Notebook.append_page( scroll[scroll_aba], titulo_da_aba);
+	lista.show_all();
+	show_all_children();
+	m_Notebook.set_current_page(indice_nova_aba);
 }
 
 void Gvlogs::abrir_seletor_de_arquivos()
 {
-  Gtk::FileChooserDialog dialog("Please choose a file", Gtk::FILE_CHOOSER_ACTION_OPEN);
-  dialog.set_transient_for(*this);
-
-  //Add response buttons the the dialog:
-  dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
-  dialog.add_button("_Open", Gtk::RESPONSE_OK);
-
-  auto filter_any = Gtk::FileFilter::create();
-  filter_any->set_name("Any files");
-  filter_any->add_pattern("*");
-  dialog.add_filter(filter_any);
-
-  //Show the dialog and wait for a user response:
-  int result = dialog.run();
-
-  //Handle the response:
-  std::string filename = "";
-  switch(result)
-  {
-      case(Gtk::RESPONSE_OK):
-      {
-        std::cout << "Open clicked." << std::endl;
-
-        auto arq = dialog.get_file();
-        nome_arquivo = arq->get_basename();
-
-        //Notice that this is a std::string, not a Glib::ustring.
-        filename = dialog.get_filename();
-        caminho_completo_arquivo = filename;
-        break;
-      }
-      case(Gtk::RESPONSE_CANCEL):
-      {
-        std::cout << "Cancel clicked." << std::endl;
-        break;
-      }
-      default:
-      {
-        std::cout << "Unexpected button clicked." << std::endl;
-        break;
-      }
-  }
+	Gtk::FileChooserDialog dialog("Please choose a file", Gtk::FILE_CHOOSER_ACTION_OPEN);
+	dialog.set_transient_for(*this);
+	
+	//Add response buttons the the dialog:
+	dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
+	dialog.add_button("_Open", Gtk::RESPONSE_OK);
+	
+	auto filter_any = Gtk::FileFilter::create();
+	filter_any->set_name("Any files");
+	filter_any->add_pattern("*");
+	dialog.add_filter(filter_any);
+	
+	//Show the dialog and wait for a user response:
+	int result = dialog.run();
+	
+	//Handle the response:
+	std::string filename = "";
+	switch(result)
+	{
+		case(Gtk::RESPONSE_OK):
+		{
+			//~ std::cout << "Open clicked." << std::endl;
+			auto arq = dialog.get_file();
+			nome_arquivo = arq->get_basename();
+			//Notice that this is a std::string, not a Glib::ustring.
+			filename = dialog.get_filename();
+			caminho_completo_arquivo = filename;
+		break;
+		}
+		case(Gtk::RESPONSE_CANCEL):
+		{
+			//~ std::cout << "Cancel clicked." << std::endl;
+			break;
+		}
+		default:
+		{
+			//~ std::cout << "Unexpected button clicked." << std::endl;
+			break;
+		}
+	}
 }
 
 vector<Glib::ustring> Gvlogs::linhas_arquivo(std::string& arquivo)
 {
-    std::ifstream file(arquivo);
-    std::string str;
-    vector<Glib::ustring> linhas;
-    while (std::getline(file, str))
-    {
-      linhas.push_back(str);
-    }
-    return linhas;
+	std::ifstream file(arquivo);
+	std::string str;
+	vector<Glib::ustring> linhas;
+	while (std::getline(file, str))
+	{
+		linhas.push_back(str);
+	}
+	return linhas;
 }
 
-// void Gvlogs::vector_delete(int index)
-// {
-//     if (index < 0 || index >= MAXIMO_ABAS)
-//         return;
-//
-//     // scroll[index] = NULL;
-//
-//     for (int i = index; i < MAXIMO_ABAS - 1; i++) {
-//         scroll[i] = scroll[i + 1];
-//         // scroll[i + 1] = NULL;
-//     }
-// }
+void Gvlogs::add_lista(ModelColumns& columns, Glib::RefPtr<Gtk::ListStore>& refTreeModel, Glib::ustring conteudo)
+{
+	columns.rowcount++;
+	Gtk::TreeModel::Row row = *(refTreeModel->append());
+	row[columns.col_cnt] = columns.rowcount;
+	row[columns.col_text] = conteudo;
+}
+
+int Gvlogs::add_lista_em_aba(vector<Glib::ustring>& linhas)
+{
+	int indice_aba = 0;
+	for(int i=0; i<MAXIMO_ABAS; i++){
+		if(
+			scroll[i].get_parent() == nullptr && 
+			treeview[i].get_parent() == nullptr
+		){
+			indice_aba = i; break;
+		}
+	}
+	  
+	scroll[ indice_aba ].set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS);
+	treeview[ indice_aba ].set_hexpand(true);
+	treeview[ indice_aba ].set_vexpand(true);
+	
+	ModelColumns columns;
+	Glib::RefPtr<Gtk::ListStore> refTreeModel;
+	
+	refTreeModel = Gtk::ListStore::create(columns);
+	treeview[ indice_aba ].set_model(refTreeModel);
+	treeview[ indice_aba ].append_column("Cnt", columns.col_cnt);
+	treeview[ indice_aba ].append_column("Text", columns.col_text);
+	
+	for(vector<Glib::ustring>::iterator i = linhas.begin(); i != linhas.end(); i++){
+		add_lista(columns, refTreeModel, *i );
+	}
+	scroll[ indice_aba ].add( treeview[ indice_aba ] );
+	columns.rowcount = 0;
+	return indice_aba;
+}
--- gvlogs.h
+++ gvlogs.h
@@ -19,8 +19,6 @@
 #ifndef GTKMM_EXAMPLE_HELLOWORLD_H
 #define GTKMM_EXAMPLE_HELLOWORLD_H
 
-// #include <gtkmm/button.h>
-// #include <gtkmm/window.h>
 #include <gtkmm.h>
 #include <iostream>
 #include <fstream>
@@ -32,93 +30,97 @@ using namespace std;
 
 class Gvlogs : public Gtk::Window
 {
+	
+	public:
+		Gvlogs();
+		virtual ~Gvlogs();
+	
+	protected:
+		//Signal handlers:
+		void evento_botao_add_arquivo();
+		void evento_botao_remover_aba();
+		void evento_trocou_aba(Gtk::Widget* page, guint page_num);
+		void on_button_quit();
+		
+		//Member widgets:
+		Gtk::Button m_button;
+		Gtk::Button add_arquivo;
+		Gtk::Button remover_aba;
+		
+		Glib::ustring titulo_primeira_aba = "File.log";
+		int altura = 700;
+		int largura = 1000;
+		std::string nome_arquivo;
+		std::string caminho_completo_arquivo;
+		
+		//Child widgets:
+		Gtk::Box m_VBox;
+		Gtk::Notebook m_Notebook;
+		Gtk::ListBox lista;
+		Gtk::ScrolledWindow scroll[MAXIMO_ABAS];
+		Gtk::TreeView treeview[MAXIMO_ABAS];
+		Gtk::ButtonBox m_ButtonBox;
+		Gtk::Button m_Button_Quit;
+		
+		// metodos
+		void add_aba(Glib::ustring, vector<Glib::ustring>*);
+		void abrir_seletor_de_arquivos();
+		vector<Glib::ustring> linhas_arquivo(std::string&);
+		void vector_delete(int);
+		int add_lista_em_aba(vector<Glib::ustring>&);
+		
+		class ModelColumns : public Gtk::TreeModel::ColumnRecord
+		{
+			public:
+			ModelColumns()
+			{ add(col_cnt); add(col_text); }
+		
+			int rowcount = 0;
+			Gtk::TreeModelColumn<int> col_cnt;
+			Gtk::TreeModelColumn<Glib::ustring> col_text;
+		};
 
-public:
-  Gvlogs();
-  virtual ~Gvlogs();
-
-protected:
-  //Signal handlers:
-  void evento_botao_add_arquivo();
-  void evento_botao_remover_aba();
-  void evento_trocou_aba(Gtk::Widget* page, guint page_num);
-  void on_button_quit();
-
-  //Member widgets:
-  Gtk::Button m_button;
-  Gtk::Button add_arquivo;
-  Gtk::Button remover_aba;
-
-  Glib::ustring titulo_primeira_aba = "File.log";
-  int altura = 700;
-  int largura = 1000;
-
-  //Child widgets:
-  Gtk::Box m_VBox;
-  Gtk::Notebook m_Notebook;
-
-
-  // Glib::RefPtr<Gio::ListStore<Glib::ustring>> m_store;
-  Gtk::Label abas[MAXIMO_ABAS];
-  Gtk::ListBox lista;
-  Gtk::ScrolledWindow scroll[MAXIMO_ABAS];
-  vector<Gtk::ScrolledWindow> v_scroll;
-
-  Gtk::ButtonBox m_ButtonBox;
-  Gtk::Button m_Button_Quit;
-
-  void add_aba(Glib::ustring, vector<Glib::ustring>*);
-  void abrir_seletor_de_arquivos();
-  vector<Glib::ustring> linhas_arquivo(std::string&);
-  void vector_delete(int);
-
-  std::string nome_arquivo;
-  std::string caminho_completo_arquivo;
-
-  //---******************************************************---
-  Gtk::TreeView treeview[MAXIMO_ABAS];
-  class ModelColumns : public Gtk::TreeModel::ColumnRecord
-        {
-            public:
-                ModelColumns()
-                { add(col_cnt); add(col_text); }
-
-                int rowcount = 0;
-                Gtk::TreeModelColumn<int> col_cnt;
-                Gtk::TreeModelColumn<Glib::ustring> col_text;
-        };
-
-        ModelColumns columns;
-        Glib::RefPtr<Gtk::ListStore> refTreeModel;
-
-  void add_lista(Glib::ustring conteudo)
-  {
-    columns.rowcount++;
-      Gtk::TreeModel::Row row = *(refTreeModel->append());
-       row[columns.col_cnt] = columns.rowcount;
-       row[columns.col_text] = conteudo;
-  }
-
-  void add_lista_em_aba(int indice_aba, vector<Glib::ustring>& linhas)
-  {
-    scroll[ indice_aba ].set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS);
-    treeview[ indice_aba ].set_hexpand(true);
-    treeview[ indice_aba ].set_vexpand(true);
-
-    refTreeModel = Gtk::ListStore::create(columns);
-    treeview[ indice_aba ].set_model(refTreeModel);
-    treeview[ indice_aba ].append_column("Cnt", columns.col_cnt);
-    treeview[ indice_aba ].append_column("Text", columns.col_text);
-
-    for(vector<Glib::ustring>::iterator i = linhas.begin(); i != linhas.end(); i++){
-        add_lista( *i );
-    }
-    scroll[ indice_aba ].add( treeview[ indice_aba ] );
-    // v_scroll.push_back( scroll[ indice_aba ] );
-    columns.rowcount = 0;
-  }
-  //---******************************************************---
-
+		void add_lista(ModelColumns&, Glib::RefPtr<Gtk::ListStore>&, Glib::ustring);
+		//~ void add_lista(ModelColumns& columns, Glib::RefPtr<Gtk::ListStore>& refTreeModel, Glib::ustring conteudo)
+		//~ {
+			//~ columns.rowcount++;
+			//~ Gtk::TreeModel::Row row = *(refTreeModel->append());
+			//~ row[columns.col_cnt] = columns.rowcount;
+			//~ row[columns.col_text] = conteudo;
+		//~ }
+		
+		//~ int add_lista_em_aba(vector<Glib::ustring>& linhas)
+		//~ {
+			//~ int indice_aba = 0;
+			//~ for(int i=0; i<MAXIMO_ABAS; i++){
+				//~ if(
+					//~ scroll[i].get_parent() == nullptr && 
+					//~ treeview[i].get_parent() == nullptr
+				//~ ){
+					//~ indice_aba = i; 
+					//~ break;
+				//~ }
+			//~ }
+			  //~ 
+			//~ scroll[ indice_aba ].set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS);
+			//~ treeview[ indice_aba ].set_hexpand(true);
+			//~ treeview[ indice_aba ].set_vexpand(true);
+			//~ 
+			//~ ModelColumns columns;
+	        //~ Glib::RefPtr<Gtk::ListStore> refTreeModel;
+	        //~ 
+			//~ refTreeModel = Gtk::ListStore::create(columns);
+			//~ treeview[ indice_aba ].set_model(refTreeModel);
+			//~ treeview[ indice_aba ].append_column("Cnt", columns.col_cnt);
+			//~ treeview[ indice_aba ].append_column("Text", columns.col_text);
+			//~ 
+			//~ for(vector<Glib::ustring>::iterator i = linhas.begin(); i != linhas.end(); i++){
+				//~ add_lista(columns, refTreeModel, *i );
+			//~ }
+			//~ scroll[ indice_aba ].add( treeview[ indice_aba ] );
+			//~ columns.rowcount = 0;
+			//~ return indice_aba;
+		//~ }
 };
 
 #endif // GTKMM_EXAMPLE_HELLOWORLD_H
